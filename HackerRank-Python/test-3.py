60e54d01e0e9666a98406d4111d47e305a3bc121
Joao Victor <victorjv3288@gmail.com>
2022-05-30 17:48:14 -0300

    atualização

---
+++ HackerRank-Python/test-3.py
@@ -0,0 +1,8 @@
+# Arithmetic Operators
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
---
+++ HackerRank-Python/test-4.py
@@ -0,0 +1,10 @@
+# Python Division
+
+a = int(input('Enter a number: '))
+b = int(input('Enter another number: '))
+
+n1 = a // b
+n2 = a / b
+
+print('a // b = ', n1)
+print('a / b = ', n2)
