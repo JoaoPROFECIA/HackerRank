4a1ec17a337642f7ecc100488ec3394e06275a64
Joao Victor <victorjv3288@gmail.com>
2022-05-30 19:29:48 -0300

    atualização

---
+++ HackerRank-Python/test-7.py
@@ -0,0 +1,6 @@
+# Print Function
+
+if __name__ == '__main__':
+    n = int(input())
+    for c in range(n):
+        print(c + 1, end = '')
---
+++ HackerRank-Python/test-8.py
@@ -0,0 +1,13 @@
+# List Comprehensions
+
+if __name__ == '__main__':
+    x = int(input('Enter the number of rows: '))
+    y = int(input('Enter the number of columns: '))
+    z = int(input('Enter the number of stars: '))
+    n = int(input('Enter the number of spaces: '))
+    print([[i, j, k] 
+    for i in range(x + 1) 
+        for j in range(y + 1) 
+            for k in range(z + 1) 
+                if i + j + k != n])
+                
