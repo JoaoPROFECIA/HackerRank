06d76fa6586b9fd74d061d46b4782138c8190b18
Joao Victor <victorjv3288@gmail.com>
2022-05-31 20:49:55 -0300

    Wheater Observation Station 11

--- HackerRank-SQL/test-16.sql
+++ HackerRank-SQL/test-16.sql
@@ -23,4 +23,5 @@ ORDER BY CITY;
 --------------------------------------------------------------------------------
 SELECT DISTINCT CITY 
 FROM STATION 
-WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u') OR LOWER(SUBSTR(CITY, LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');   
+WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u') 
+OR LOWER(SUBSTR(CITY,LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');
