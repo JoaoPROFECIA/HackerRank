84110ebe096773a9ef0ae90171503e197cd6cf54
Joao Victor <victorjv3288@gmail.com>
2022-05-31 12:15:18 -0300

    Select All

--- HackerRank-SQL/test-3.sql
+++ HackerRank-SQL/test-3.sql
@@ -1,6 +1,9 @@
 /*
 Select All
 
+Query all columns (attributes) for every row in the CITY table.
+The CITY table is described as follows:
+            CITY
 +-------------+--------------+
 | Field       | Type         |
 +-------------+--------------+
