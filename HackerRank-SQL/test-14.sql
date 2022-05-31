bfa10993ef982133a897e73538ba0ac84c77bdec
Joao Victor <victorjv3288@gmail.com>
2022-05-31 17:05:22 -0300

    Wheater Observation Station 9

---
+++ HackerRank-SQL/test-14.sql
@@ -0,0 +1,22 @@
+/*
+Wheater Observation Station 9
+
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+           STATION
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| CITY        | VARCHAR2(21) |
+| STATE       | VARCHAR2(2)  |
+| LAT_N       | NUMBER       |
+| LONG_W      | NUMBER       |
++-------------+--------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[^aeiouAEIOU]'
+ORDER BY CITY;
