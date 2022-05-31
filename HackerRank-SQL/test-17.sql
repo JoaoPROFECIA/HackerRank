f17ffc099baafdc8c9a4b0dae92df3d291114037
Joao Victor <victorjv3288@gmail.com>
2022-05-31 20:41:00 -0300

    Wheater Observation Station 12

---
+++ HackerRank-SQL/test-17.sql
@@ -0,0 +1,26 @@
+/*
+Wheater Observation Station 12
+
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+           STATION
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| CITY        | VARCHAR2(21) |
+| STATE       | VARCHAR2(2)  |
+| LAT_N       | NUMBER       |
+| LONG_W      | NUMBER       |
++-------------+--------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP "^[^aeiou].+" AND CITY REGEXP ".+[^aeiou]$"
+ORDER BY CITY;
+--------------------------------------------------------------------------------
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u') AND LOWER(SUBSTR(CITY,LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');    
