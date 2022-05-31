6446d5ee98e054b27b7bfb01090d2fc06489b7db
Joao Victor <victorjv3288@gmail.com>
2022-05-31 12:03:26 -0300

    Japanese Cities' Names

---
+++ HackerRank-SQL/test-6.sql
@@ -0,0 +1,17 @@
+/*
+Japanese Cities' Names
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
