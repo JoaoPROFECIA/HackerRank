64951069011026537f097f1b82c14e630f058336
Joao Victor <victorjv3288@gmail.com>
2022-05-31 11:56:06 -0300

    Japanese Cities Attributes

---
+++ HackerRank-SQL/test-5.sql
@@ -0,0 +1,17 @@
+/*
+Japanese Cities Attributes
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
