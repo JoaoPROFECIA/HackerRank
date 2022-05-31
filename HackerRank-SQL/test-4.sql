63d386bdad97c424ac56ba63de76e95058163a77
Joao Victor <victorjv3288@gmail.com>
2022-05-31 12:15:28 -0300

    Select By ID

--- HackerRank-SQL/test-4.sql
+++ HackerRank-SQL/test-4.sql
@@ -1,6 +1,9 @@
 /*
 Select By ID
 
+Query all columns for a city in CITY with the ID 1661.
+The CITY table is described as follows:
+            CITY
 +-------------+--------------+
 | Field       | Type         |
 +-------------+--------------+
