5b892dc94cfe0618a24d67da0355256e51f7bf2b
Joao Victor <victorjv3288@gmail.com>
2022-05-31 20:48:09 -0300

    Wheater Observation Station 10

--- HackerRank-SQL/test-15.sql
+++ HackerRank-SQL/test-15.sql
@@ -20,3 +20,8 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '[^aeiouAEIOU]$'
 ORDER BY CITY;
+--------------------------------------------------------------------------------
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE UPPER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('A','E','I','O','U') 
+AND LOWER(SUBSTR(CITY, LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');
