f0b73c5ae6cdd7f764ad7437fc8ff165647e6669
Joao Victor <victorjv3288@gmail.com>
2022-05-31 11:51:45 -0300

    Select By ID

---
+++ HackerRank-SQL/test-4.sql
@@ -0,0 +1,17 @@
+/*
+Select By ID
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+SELECT *
+FROM CITY
+WHERE ID=1661; --(OU ID = '1661';)
