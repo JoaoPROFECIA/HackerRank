c2878ebabb1aa8528ac1cb3de1e80e517c051fb5
Joao Victor <victorjv3288@gmail.com>
2022-06-03 15:14:56 -0300

    Finding the percentage

---
+++ HackerRank-Python/test-11.py
@@ -0,0 +1,17 @@
+# Finding the percentage
+
+if __name__ == '__main__':
+    def percentage():
+        n = int(input())
+        student_marks = {}
+        for _ in range(n):
+            name, *line = input().split()
+            scores = list(map(float, line))
+            student_marks[name] = scores
+        query_name = input()
+        query_scores = student_marks[query_name]
+        avg = sum(query_scores) / 3.0
+        print("%.2f" % avg)
+
+        
+    percentage()
